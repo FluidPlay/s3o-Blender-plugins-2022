b0bc4f70a289a359a850bc895a0ffff310cbefd7
René Jochum <rene@jochum.dev>
2023-12-17 02:19:51 +0100

    fix: optimize s3o_open.sh

--- scripts/s3o_open.sh
+++ scripts/s3o_open.sh
@@ -2,13 +2,7 @@
 
 # Install: sudo ln -s $PWD/s3o_open.sh /usr/local/bin/blender-s3o
 
-SOURCE=${BASH_SOURCE[0]}
-while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
-  SOURCE=$(readlink "$SOURCE")
-  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-SCRIPT_DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+SCRIPT_DIR=$( cd -P "$( dirname $( readlink "${BASH_SOURCE[0]}" ) )" >/dev/null 2>&1 && pwd )
 
 if [[ ! -f "$1" ]]; then
     echo "Usage: blender-s3o <file.s3o>"
